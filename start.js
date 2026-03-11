d3aab7506f506412107c0169514d3b86b92a2122
reas-dtech <24582rd@hvhs.school.nz>
2026-03-11 15:10:29 +1300

    got something working

--- start.js
+++ start.js
@@ -1,66 +1,40 @@
-const Interaction = {
-    Inside: "Inside",
-    Outside: "Outside",
-}
-class Menu {
-    constructor(x, y, w, h, items) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.items = [...items];
-    }
-    addItem(item) {
-        this.items.push(item);
-    }
-    draw()
-{
-    push();
-    translate(this.x, this.y);
-    stroke(0);
-    fill(255);
-    rect(0, 0, this.w, this.h);
-    for (let i = 0; i < this.items.length; i++) {
-        {
-            this.items[i].draw();
-        }
-        pop();
-    }
-    interact(mx, my)
-    {
-        for (let i = this.items.length; i > 0; i--) {
-            let item = this.items[i];
-            let inter = item.interact(mx - this.x, my - this.y);
-            if (inter == Interaction.Inside) return item;
-        }
-        return null;
-    }
-    shift(dx, dy)
-    {
-        this.x += dx;
-        this.y += dy;
-    }
-
-}
-// was working right here on a menu using a tutorial
-
-class Item {
-    constructor(x, y, w, h,) {
-
-
-
-    }
-
-
-
-
-
-
-}
 function setup() {
-    cnv = new Canvas(windowWidth, windowHeight);
-
+    createCanvas(windowWidth, windowHeight);
 }
+let gameState = 'start';
 function draw() {
     background(220);
+    if (gameState === 'start') {
+        displaystartScreen();
+}
+ else if (gameState === 'play') {
+    playGame();
+ }
+ else if (gameState === 'gameOver') {
+    displayGameOverScreen();
+ }
+}
+// improve the start screen 
+function displaystartScreen() {
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    text('Welcome to catching, click to start', width / 2, height / 2);
+}
+
+function playGame() {
+// game code goes here just has the text for now
+    text('game', width / 2, height / 2);
+}
+function displayGameOverScreen() {
+    text('Game Over', width / 2, height / 2);
+}
+function mousePressed() {
+    if (gameState === 'start' || gameState === 'gameOver') {
+        gameState = 'play';
+        // add to reset game variables if needed
+    }
+    
+}
+// maybe add if a key is press to do something on the menu
+function keyPressed() {
 }
