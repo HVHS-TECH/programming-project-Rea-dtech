a7d9989f5dc0a7d52569b5d7d94dd91a306c2d9b
reas-dtech <24582rd@hvhs.school.nz>
2026-03-13 09:57:55 +1300

    working on movement

--- start.js
+++ start.js
@@ -1,28 +1,52 @@
+let img;
+let seaGull;
+function preload() {
+    img = loadImage('seagull.png');
+}
+
 function setup() {
+
     createCanvas(windowWidth, windowHeight);
+
 }
 let gameState = 'start';
 function draw() {
     background(220);
     if (gameState === 'start') {
         displaystartScreen();
+    }
+    else if (gameState === 'play') {
+        playGame();
+    }
+    else if (gameState === 'gameOver') {
+        displayGameOverScreen();
+    }
+    if (kb.pressing('left')) {
+
+        seaGull.rotate(-10);
+    }
+    if (kb.pressing('right')) {
+        
+        seaGull.rotate(10);
+    }
+    if (kb.pressing('up')) {
+    // work out how to get it to move
+    }
+    if (kb.pressing('down')) {
+      // same thing
+    }
 }
- else if (gameState === 'play') {
-    playGame();
- }
- else if (gameState === 'gameOver') {
-    displayGameOverScreen();
- }
-}
-// improve the start screen 
+// improve the start screen and think of a better game name
 function displaystartScreen() {
     textAlign(CENTER, CENTER);
     textSize(20);
-    text('Welcome to catching, click to start', width / 2, height / 2);
+    text('Welcome to Fishing, click to start, full screen is recommended (press F11 or the full screen button on your keyboard)', width / 2, height / 2);
 }
 
 function playGame() {
-// game code goes here just has the text for now
+    // game code goes here just has the text for now
+  
+
     text('game', width / 2, height / 2);
 }
 function displayGameOverScreen() {
@@ -31,9 +55,12 @@ function displayGameOverScreen() {
 function mousePressed() {
     if (gameState === 'start' || gameState === 'gameOver') {
         gameState = 'play';
+        seaGull = new Sprite(width / 2, height / 2, 10, 10);
+            seaGull.img = img;
+        seaGull.scale = 0.5;
         // add to reset game variables if needed
     }
-    
+
 }
 // maybe add if a key is press to do something on the menu
 function keyPressed() {
