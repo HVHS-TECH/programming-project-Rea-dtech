e88bf573d5d6c82350218ff76a0be90d2ab8e209
reas-dtech <24582rd@hvhs.school.nz>
2026-03-17 12:27:41 +1300

    working on hitboxes

--- start.js
+++ start.js
@@ -1,16 +1,19 @@
-let img;
+let gullImg;
 let seaGull;
 let fish;
 let oceanBackground;
+let score;
+let fishImg;
 function preload() {
-    img = loadImage('seagull.png');
+    gullImg = loadImage('seagull.png');
     oceanBackground = loadImage('image.png')
+    fishImg = loadImage('fish.png')
 }
 
 function setup() {
 
     createCanvas(windowWidth, windowHeight);
-
+    
 }
 let gameState = 'start';
 function draw() {
@@ -25,6 +28,8 @@ function draw() {
     else if (gameState === 'gameOver') {
         displayGameOverScreen();
     }
+
+
     /*
     ATTEMPT AT KBM MOVEMENT MAY USE
     if (kb.pressing('left')) {
@@ -48,15 +53,21 @@ function draw() {
 function displaystartScreen() {
     textAlign(CENTER, CENTER);
     textSize(20);
-    text('Welcome to Fishing, click to start (press F11 or the full screen button on your keyboard if you would prefer full screen then reload the page )', width / 2, height / 2);
+    text(
+        'Welcome to Fishing, click to start (press F11 or the full screen button on your keyboard if you would prefer full screen then reload the page in order to eat the fish you have to get it with the beak of the seagull)',
+        width / 2,
+        height / 2
+    );
 }
 
 function playGame() {
     // game code goes here just has the text for now
-
+    if (seaGull.collides(fish)) {
+        fish.remove();
+    }
     seaGull.moveTowards(mouse, 0.20);
     seaGull.rotateTowards(mouse, 1, -100);
-    text('game', width / 2, height / 2);
+
 }
 function displayGameOverScreen() {
     text('Game Over', width / 2, height / 2);
@@ -64,14 +75,21 @@ function displayGameOverScreen() {
 function mousePressed() {
     if (gameState === 'start' || gameState === 'gameOver') {
         gameState = 'play';
-        seaGull = new Sprite(width / 2, height / 2, 10, 10);
-        fish = new Sprite()
-        seaGull.img = img;
+        
+        seaGull = new Sprite(width / 2, height / 2, 100, 100);
+        fish = new Sprite(500, 500, 10, 10)
+        fish.setCollider("rectangle", 0, 0, 50, 50)
+        seaGull.img = gullImg;
         seaGull.scale = 0.5;
+        fish.scale = 0.35
+        fish.img = fishImg;
+        seaGull.debug = true;
+        fish.debug = true;
+       
         // add to reset game variables if needed
     }
 
 }
 // maybe add if a key is press to do something on the menu
 function keyPressed() {
-}
+} 
