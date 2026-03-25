cdb28bd5ae0a0f4449b5fca13d5f7a802ef7319b
reas-dtech <24582rd@hvhs.school.nz>
2026-03-25 15:14:44 +1300

    did a bunch of stuff

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 //fix copyright for imgs
-
+const credits = "seagull and fish art: freepik.com";
+let lives = 3;
 let gullImg;
 let seaGull;
 let fish;
@@ -30,6 +31,14 @@ function draw() {
         textSize(20);
         textAlign(LEFT);
         text('Score: ' + score, 10, 30);
+        text('Lives: ' + lives, 10, 60);
+        // red dots to indicate lives
+        for (let i = 0; i < lives; i++) {
+            fill(255, 0, 0);
+            noStroke();
+            ellipse(20 + i * 25, 90, 15, 15);
+        }
+        fill(255); // reset fill for other text
     }
     else if (gameState === 'gameOver') {
         displayGameOverScreen();
@@ -60,13 +69,23 @@ function draw() {
 // improve the start screen and think of a better game name
 function displaystartScreen() {
     textAlign(CENTER, CENTER);
-    textSize(20);
+    textSize(24);
+    fill(255)
     text(
-        'Welcome to Fishing, click to start (press F11 or the full screen button on your keyboard if you would prefer full screen then reload the page in order to eat the fish you have to get it with the beak of the seagull)',
+        'Welcome to Fishing, click to start ',
+
         width / 2,
-        height / 2
+        height / 3
     );
+   
+    textAlign(CENTER, CENTER);
+    textSize(20.5);
+    fill(255);
+    text('(press F11 or the full screen button on your keyboard if you would prefer full screen then reload the page in order to eat the fish you have to get it with the beak of the seagull)', width / 2, height / 3 + 40);
+    textSize(16);
+    text('Credits: sprites from freepik.com', width / 2, height / 3 + 80);
 }
+
 
 // spawns fish and has them go to the bottom of the screen and can be eaten
 function spawnFish() {
@@ -74,7 +93,7 @@ function spawnFish() {
         fish.remove();
     }
     fish = new Sprite(random(50, width - 10), -100, 10, 10)
-    fish.vel.y = random(6, 20);
+    fish.vel.y = random(10, 25);
     fish.addCollider(0, 5, 210, 90)
     fish.scale = 0.35
     fish.img = fishImg;
@@ -86,9 +105,14 @@ function playGame() {
         score += 1;
         spawnFish();
     }
-
+    //if lives are at 0 game over and one is lost every time the fish goes through the bottom of the screen
     if (fish.y > height + 0) {
-        gameState = 'gameOver';
+        lives -= 1;
+        if (lives <= 0) {
+            gameState = 'gameOver';
+        } else {
+            spawnFish();
+        }
     }
 
     seaGull.moveTowards(mouse, 0.20);
@@ -100,7 +124,8 @@ function displayGameOverScreen() {
         seaGull.remove();
     }
     textAlign(CENTER, CENTER);
-    text('Game Over, click the mouse to play again', width / 2, height / 2);
+    text('Game Over, click the mouse to play again and you caught ' + score + ' fish', width / 2, height / 2);
+
     textSize(40)
 }
 // when the mouse is pressed on the start menu the gamestate is changed to play and loads all of the sprites
@@ -111,6 +136,7 @@ function mousePressed() {
 
         gameState = 'play';
         score = 0;
+        lives = 3;
         if (seaGull) {
             seaGull.remove();
         }
