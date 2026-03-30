cf6852b23a01d301a2e48f9982358bb502e38a09
reas-dtech <24582rd@hvhs.school.nz>
2026-03-31 12:09:08 +1300

    moved the images and updated some comments

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ const tips = [
     "try to move slowly not quickly to avoid missing the fish",
     "be prepared for a fish to spawn in at the other side of the screen",
     "your lives can go quickly so when you lose one don't just give up catch the next fish",
-    "did you know the game developers high score is 69",
+    "did you know the game developers high score is 69  ",
     "make sure your movements are controlled ",
 ];
 let currentTip;
@@ -15,10 +15,12 @@ let fish;
 let oceanBackground;
 let fishImg;
 let score = 0;
+let highScore = 0;
+let gameState = 'start';
 function preload() {
-    gullImg = loadImage('seagull.png');
-    oceanBackground = loadImage('image.png')
-    fishImg = loadImage('fish.png')
+    gullImg = loadImage('images/seagull.png');
+    oceanBackground = loadImage('images/image.png')
+    fishImg = loadImage('images/fish.png')
 }
 
 function setup() {
@@ -26,12 +28,12 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 
 }
-let gameState = 'start';
+//draw function
 function draw() {
     background(oceanBackground);
 
     if (gameState === 'start') {
-        displaystartScreen();
+        displayStartScreen();
 
     }
     else if (gameState === 'play') {
@@ -40,7 +42,7 @@ function draw() {
         textAlign(LEFT);
         text('Score: ' + score, 10, 30);
         text('Lives: ' + lives, 10, 60);
-        // red dots to indicate lives
+        // the red dots on the top right of your green indicate your lives and when one is the lost the lives text should go down by one and 
         for (let i = 0; i < lives; i++) {
             fill(255, 0, 0);
             noStroke();
@@ -53,16 +55,22 @@ function draw() {
 
 
     }
+    if (score > highScore) {
+    highScore = score;
+}
 
 
 }
-// improve the start screen and think of a better game name
-function displaystartScreen() {
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+// startscreen this is displayed when the game is loaded
+function displayStartScreen() {
     textAlign(CENTER, CENTER);
     textSize(24);
     fill(255)
     text(
-        'Welcome to Fishing, click to start ',
+        'Welcome to Bird catch fish, click to start ',
 
         width / 2,
         height / 3
@@ -71,7 +79,7 @@ function displaystartScreen() {
     textAlign(CENTER, CENTER);
     textSize(20.5);
     fill(255);
-    text('(To control the seagull use your mouse as the seagull follows the mouse and the goal is to catch the fish if you miss the fish then you would lose a live)', width / 2, height / 3 + 40);
+    text('(To control the seagull use your mouse as the seagull follows the mouse and the goal is to catch the fish if you miss the fish then you would lose a live and if you lose all 3 lives then game over )', width / 2, height / 3 + 40);
     textSize(16);
     text('Credits: sprites from freepik.com', width / 2, height / 3 + 80);
 }
@@ -91,6 +99,8 @@ function spawnFish() {
 }
 function playGame() {
     // game code goes here and we have the code for the fish being eaten and the seagulls movement
+   /* if the seagull touches the fish then the score is increased by one
+   and the spawnFish function is called to spawn a new one */
     if (seaGull.collides(fish)) {
         score += 1;
         spawnFish();
@@ -119,11 +129,10 @@ function displayGameOverScreen() {
 
     textSize(20);
     text('Tip: ' + currentTip, width / 2, height / 2 + 50);
-
+    text('highscore = ' + highScore, width / 2, height / 2 + 100)
     textSize(40)
 }
-// when the mouse is pressed on the start menu the gamestate is changed to play and loads all of the sprites
-
+/* when the mouse is pressed on the start menu the gamestate is changed to play and loads all of the sprites and runs the spawnFish function and the score and lives */
 
 function mousePressed() {
     if (gameState === 'start' || gameState === 'gameOver') {
@@ -140,7 +149,6 @@ function mousePressed() {
         seaGull.scale = 0.5;
         seaGull.addCollider(0, 20, 50, 50)
 
-        // add to reset game variables if needed
     }
 }
 
